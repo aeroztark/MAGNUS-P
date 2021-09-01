bd2bb5376d25816acabb84808806f5046a25d499
Sarthak Srivastava <sarthak002@e.ntu.edu.sg>
2021-09-01 23:31:57 +0800

    created ref folder
    ref folder will have a reference copy of the code

---
+++ test/ref/Earth_isothermal.m
@@ -0,0 +1,26 @@
+function[T,rho,p,R,gamma,kvisc,thermdiffus,H,C] = Earth_isothermal(z_array)
+
+% Earth isothermal model to provide atmospheric state
+% input: z_array -> Z x X array 
+% all outputs have the same dimension as z_array
+
+T_surface = 239;
+p_surface = 1E5;
+rho_surface = 1.2;
+gamma = 1.4;    %constant gamma
+R = 287;
+Pr = 0.7;
+
+H = p_surface/(rho_surface*9.8); % scaled height
+
+p = p_surface*exp(-z_array./H);
+rho = rho_surface*exp(-z_array./H);
+T = T_surface.*ones(size(z_array));
+R = R.*ones(size(z_array));
+C = sqrt(gamma*R.*T);
+C = C.*ones(size(z_array));
+H = H.*ones(size(z_array));
+gamma = gamma.*ones(size(z_array));
+%N = sqrt(gamma-1)*9.8/C;
+kvisc = (3.5e-7)*(T.^0.69)./rho; % Banks & Kockarts, 1973
+thermdiffus = kvisc./Pr;
---
+++ test/ref/config_baseline.m
@@ -0,0 +1,92 @@
+% Config file for baseline simulations (for testing purposes)
+
+%% ---- Time settings ----
+Tmin  = 0;    % Initial time
+Tmax  = 5000; % Final time in seconds
+skipT = 30;  % Number of seconds to skip storing results
+% computation happens after every dt but only limited data is stored
+n = 0;       % First Step n = 0 (n counts the current stored frame)
+t = Tmin;    % First time t = Tmin
+nframe = 1;  % First frame = 1 (nframe is the total no of stored frames)
+T_arr(nframe) = 0; % T_arr is the time array of stored frames
+
+%% ---- Viscous phenomenon flags ----
+IsTopSpongeLayer = 0; % flag to include a sponge layer on top (20 km gives good results)
+IsViscosity = 1;% flag to solve for molecular viscosity
+IsConduction = 1; % flag to solve for thermal conduction  
+IsDiffusionImplicit = 0;
+%% ---- Domain settings ----
+% note on indexing: X(row,col) --> X(z_ind, x_ind)
+Xmin = -20000;
+Xmax = 20000;
+Zmin = 0;
+Zmax = 160000;
+dx = 500; % horizontal resolution
+dz = 500; % vertical resolution
+SpongeHeight = 20000; % sponge layer thickness in meters
+
+ZDomainEnd = Zmax; % last value of Z for ' physically valid' domain (pre-sponge)
+
+if IsTopSpongeLayer == 1
+    Zmax = Zmax + SpongeHeight;    % extend Z by 50 km for computations
+end
+
+Xdomain = Xmax-Xmin;
+Zdomain = Zmax-Zmin;
+% Define positions at cell centers
+x_c = Xmin-3*dx/2:dx:Xmax+3*dx/2;   %2 centre points outside on either boundary
+z_c = Zmin-3*dz/2:dz:Zmax+3*dz/2;
+[X,Z] = meshgrid(x_c,z_c);    % grid of cell centers
+[J,I] = size(X);  %J gives no of z levels and I gives no of x levels
+
+%% ---- CFL ----
+dCFL = 0.8; % desired Courant-Friedrichs-Lewy number
+difCFL = 0.2; % CFL for diffusion problem
+
+%% ---- Background atmosphere ----
+global g R P0 rho0 gamma C; 
+
+% If using Earth isothermal model
+[T0,rho0,P0,R,gamma,kinvisc,thermdiffus,H,C] = Earth_isothermal(Z);
+ 
+% If using Earth MSIS model
+%[T0,rho0,P0,R,gamma,kinvisc,thermdiffus,H,C,~,~] = Earth_MSIS(Z,10,180,2020,1,0);
+
+%% ---- Background wind ----
+% only horizontal wind is specified -> time invariant. Vertical wind is zero.
+% will need recheck equations to subtract from rho*u and rho*w if this changes 
+
+global wind
+% Gaussian wind shear
+u_max = 0;    % wind amplitude (m/s) 
+u_zloc = 100000;    % z location of wind peak (m)
+u_sig = 10000;    % stdev of wind profile (m)
+wind = u_max.*exp(-(Z-u_zloc).^2./(2*u_sig^2));    % wind profile, also a matrix of size X=Z
+
+% linear wind shear
+% wind = linspace(0,u_max,length(z_c));
+% wind = repmat(wind',1,length(x_c));    % also a matrix of size X=Z
+
+%% ---- Wave forcing ----
+% A lower boundary Source is simulated as Gaussian w perturbation
+global forcing
+forcing.thermal = false;     %if thermal forcing is applied
+forcing.verticalvelocity = true;    
+
+if forcing.thermal
+    forcing.amp = 100;      % amplitude (K/s), typically of form: A x Cp
+    forcing.x0 = 0;  %  forcing center x location (m)
+    forcing.sigmax = 500;     % forcing half width x (m)
+    forcing.z0 = 20000; %  forcing center z location (m)
+    forcing.sigmaz = 1000;     % forcing half width z (m)
+    forcing.t0 = 1200;      % time at forcing maxima (s)
+    forcing.sigmat = 600;     % forcing half width time (s)
+else
+    % parameters for vertical velocity type forcing
+    forcing.amp = 0.001;      % amplitude (m/s)
+    forcing.omega = 0.007;  % centered frequency
+    kx = 2*pi / (Xmax-Xmin);    % One horizontal wavelength per domain is set (lambda_x = x domain length)
+    forcing.kxx = x_c.*kx;  % computing kx*x
+    forcing.t0 = 1200;      % time at forcing maxima (s)
+    forcing.sigmat=600;     % forcing half width time (s)
+end
