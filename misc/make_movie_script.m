e50ac8db238a06fb5ec2077647dacb6a2ee3f63e
Sarthak Srivastava <sarthak002@e.ntu.edu.sg>
2022-02-06 12:50:42 +0800

    Improved moviemaking function
    Updated function to display movies correctly, without messed up frames. Added code to save as video.

--- misc/make_movie_script.m
+++ misc/make_movie_script.m
@@ -4,17 +4,26 @@
 loops = length(T_arr);
 M(loops) = struct('cdata',[],'colormap',[]);
 
+figure
+
 for j = 1:loops
     
     % enter the parameter here
-contourf(X_KM,Z_KM,U(:,:,j).*SCALING_FACTOR,50,'Edgecolor','none');
+
+contourf(X_KM,Z_KM,W(:,:,j).*SCALING_FACTOR,50,'Edgecolor','none');
 colorbar
 xlabel('x (km)')
 ylabel('z (km)')
 title(['scaled w (m/s) at time t=',num2str(T_arr(j)),'s'])
 drawnow
-M(j) = getframe(gcf);
+M(j) = getframe(gca);
 end
 
-% to play the movie later:
-%movie(M,1,10)
+% to playback:
+% movie(gcf,M,1,0.5)
+
+% % Save video
+% v = VideoWriter('D:\address\filename.avi');
+% open(v)
+% writeVideo(v,M)
+% close(v)
